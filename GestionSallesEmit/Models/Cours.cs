fd19ce372c53591b1202ea8a5bb071b163f8631a
Sitraka26 <rakotomalalasitraka14@gmail.com>
2026-07-13 04:57:39 +0300

    Mise à jour design et corrections

--- GestionSallesEmit/Models/Cours.cs
+++ GestionSallesEmit/Models/Cours.cs
@@ -6,11 +6,10 @@ public class Cours
 {
     [Key]
     public int Id { get; set; }
-
-    [Required(ErrorMessage = "Le nom du cours est obligatoire.")]
-    [StringLength(100, MinimumLength = 2, ErrorMessage = "Le nom du cours doit contenir entre 2 et 100 caractères.")]
-    public string NomCours { get; set; } = string.Empty;
-
+[Required(ErrorMessage = "Le nom du cours est obligatoire.")]
+[StringLength(100, MinimumLength = 2, ErrorMessage = "Le nom du cours doit contenir entre 2 et 100 caractères.")]
+[RegularExpression(@"^[A-Za-zÀ-ÖØ-öø-ÿ\s\-'#+]+$", ErrorMessage = "Le nom du cours ne doit pas contenir de chiffres.")]
+public string NomCours { get; set; } = string.Empty;
     [Range(1, 30, ErrorMessage = "Les crédits doivent être compris entre 1 et 30.")]
     public int Credit { get; set; }
 }
--- GestionSallesEmit/Models/Enseignant.cs
+++ GestionSallesEmit/Models/Enseignant.cs
@@ -6,14 +6,15 @@ public class Enseignant
 {
     [Key]
     public int Id { get; set; }
+[Required(ErrorMessage = "Le nom est obligatoire.")]
+[StringLength(50, ErrorMessage = "Le nom ne peut pas dépasser 50 caractères.")]
+[RegularExpression(@"^[A-Za-zÀ-ÖØ-öø-ÿ\s\-']+$", ErrorMessage = "Le nom ne doit contenir que des lettres.")]
+public string Nom { get; set; } = string.Empty;
 
-    [Required(ErrorMessage = "Le nom est obligatoire.")]
-    [StringLength(50, ErrorMessage = "Le nom ne peut pas dépasser 50 caractères.")]
-    public string Nom { get; set; } = string.Empty;
-
-    [Required(ErrorMessage = "Le prénom est obligatoire.")]
-    [StringLength(50, ErrorMessage = "Le prénom ne peut pas dépasser 50 caractères.")]
-    public string Prenom { get; set; } = string.Empty;
+[Required(ErrorMessage = "Le prénom est obligatoire.")]
+[StringLength(50, ErrorMessage = "Le prénom ne peut pas dépasser 50 caractères.")]
+[RegularExpression(@"^[A-Za-zÀ-ÖØ-öø-ÿ\s\-']+$", ErrorMessage = "Le prénom ne doit contenir que des lettres.")]
+public string Prenom { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "L'email est obligatoire.")]
     [EmailAddress(ErrorMessage = "Le format de l'email n'est pas valide (ex: nom@emit.mg).")]
